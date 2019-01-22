239c1352de169eb903e8bdf69c238c11fa7fd03c
Don Morgan <don.morgan@gov.bc.ca>
2019-01-22 10:45:16 -0800

    Code updates

--- 01_load.R
+++ 01_load.R
@@ -30,56 +30,48 @@ if (!file.exists(BCr_file)) {
 }
 
 #BTM - for settlement, rural, agriculture, range, mining
-BTM_file <- file.path("tmp/BTM_Brick")
-if (!file.exists(BTM_file)) {
-  # Link to BTM file download from BCDC:
-  # https://catalogue.data.gov.bc.ca/dataset/baseline-thematic-mapping-present-land-use-version-1-spatial-layer
-  #Dowload file manually and put *.zip in this script and place file in the data directory
-  BTMZip <- 'BCGW_78757263_1520272242999_7572.zip'
-  unzip(file.path(DataDir, BTMZip), exdir = file.path(DataDir, "BTM"))
-  
-  # List feature classes in the geodatabase
-  BTM_gdb <- list.files(file.path(DataDir, "BTM"), pattern = ".gdb", full.names = TRUE)[1]
-  fc_list <- st_layers(BTM_gdb)
-  BTM <- read_sf(BTM_gdb, layer = "WHSE_BASEMAPPING_BTM_PRESENT_LAND_USE_V1_SVW")
-  
-  # Pull out the BTM layers and rasterize
-  #BTM[1,] #look at file header
-  NonHab <- BTM[BTM$PRESENT_LAND_USE_LABEL %in% c('Fresh Water','Outside B.C.','Salt Water', 'Glaciers and Snow') ,] %>% 
-    fasterize(ProvRast, background=NA)
-  
-  AgricultureR <- BTM[BTM$PRESENT_LAND_USE_LABEL %in% c("Agriculture",'Residential Agriculture Mixtures') ,] %>% 
-    fasterize(ProvRast, background=0)
-  
-  RangeR <- BTM[BTM$PRESENT_LAND_USE_LABEL %in% c('Range Lands') ,] %>% 
-    fasterize(ProvRast, background=0)
-  
-  UrbanR <- BTM[BTM$PRESENT_LAND_USE_LABEL %in% c('Urban') ,] %>% 
-    fasterize(ProvRast, background=0)
-  
-  MiningR <- BTM[BTM$PRESENT_LAND_USE_LABEL %in% c('Mining') ,] %>% 
-    fasterize(ProvRast, background=0)
-  
-  RecR <- BTM[BTM$PRESENT_LAND_USE_LABEL %in% c('Recreation Activities') ,] %>% 
-    fasterize(ProvRast, background=0)
-  
-  # Write out the BTM layers as individual rasters
-  writeRaster(AgricultureR, filename=file.path(DataDir,"LandDisturbance/AgricultureR.tif"), format="GTiff", overwrite=TRUE)
-  writeRaster(RangeR, filename=file.path(DataDir,"LandDisturbance/RangeR.tif"), format="GTiff", overwrite=TRUE)
-  writeRaster(UrbanR, filename=file.path(DataDir,"LandDisturbance/Urban.tif"), format="GTiff", overwrite=TRUE)
-  writeRaster(MiningR, filename=file.path(DataDir,"LandDisturbance/Mining.tif"), format="GTiff", overwrite=TRUE)
-  writeRaster(RecR, filename=file.path(DataDir,"LandDisturbance/Rec.tif"), format="GTiff", overwrite=TRUE)
-  writeRaster(NonHab, filename=file.path(DataDir,"NonHab.tif"), format="GTiff", overwrite=TRUE)
-  
-  #Make a raster brick of layers - not using due to raster memory allocation bug
-  BTM_Brick<- brick(AgricultureR, UrbanR, MiningR, RecR)
-  names(BTM_Brick) <- c('AgricultureR','UrbanR','MiningR','RecR')
-  saveRDS(BTM_Brick, file = BTM_file)
-  
-} else {
-  BTM_Brick <- readRDS(file = BTM_file)
-  NonHab<-raster(file.path(DataDir,"NonHab.tif"))
-}  
+# read from Bear_Data repo
+AgricultureR <- raster(file.path(GBspatialDir,"AgricultureR.tif"))
+RangeR <- raster(file.path(GBspatialDir,"RangeR.tif"))
+UrbanR <- raster(file.path(GBspatialDir,"UrbanR.tif"))
+MiningR <- raster(file.path(GBspatialDir,"MiningR.tif"))
+RecR <- raster(file.path(GBspatialDir,"RecR.tif"))
+
+BTM_Brick<- brick(AgricultureR, RangeR, UrbanR, MiningR, RecR)
+names(BTM_Brick) <- c('AgricultureR','RangeR','UrbanR','MiningR','RecR')
+saveRDS(BTM_Brick, file = BTM_file)
+
+# CE data
+# GB Security Areas, Human Access and Road Density
+# from GB_Data repo: https://github.com/bcgov/GB_Data
+#Security Areas
+SecureR<-raster(file.path(GBspatialDir,"SecureR.tif"))
+
+#Human access
+FrontCountryR<-raster(file.path(GBspatialDir,"FrontCountryR.tif"))
+
+#Road Density
+RdDensR<-raster(file.path(GBspatialDir,"RdDens.tif"))
+
+#Salmon Change
+SalmonChangeR<-raster(file.path(GBspatialDir,"SalmonChangeR.tif"))
+
+#Mid Seral
+MidSeralR<-raster(file.path(GBspatialDir,"MidSeralR.tif"))
+
+# Hunter Density
+# from HunterDensity repo: https://github.com/bcgov/HunterDensity
+HuntDDensR<-raster(file.path(HunterSpatialDir,"HuntDDensR.tif"))
+HuntDDensNonHabR<-raster(file.path(HunterSpatialDir,"HuntDDensNonHabR.tif"))
+
+#Human & Livestock Denisty
+# from HumanLivestock repo: https://github.com/bcgov/HumanLivestockDensity
+HumanDensityR<-raster(file.path(HumanLivestockSpatialDir,"HumanDensityR.tif"))
+LivestockDensityR<-raster(file.path(HumanLivestockSpatialDir,"LSDensityR.tif"))
+
+#Mortlaity - see GB_unreported
+# from GB_Mortality repo: https://github.com/bcgov/GB_Mortality
+MortR<-raster(file.path(MortSpatialDir,"MortR.tif"))
 
 #Point layers for development
 #Placer and coal tenures - https://catalogue.data.gov.bc.ca/dataset/mta-mineral-placer-and-coal-tenure-spatial-view
@@ -146,166 +138,27 @@ if (!file.exists(Linear_file)) {
   saveRDS(Linear_Brick, file = Linear_file)
 } else {
   Linear_Brick<-readRDS(file = Linear_file)
-  RailR<-raster(file.path(DataDir,"LandDisturbance/RailR.tif"))
-  RdDensR<-raster(file.path(DataDir,"LandDisturbance/RdDensR.tif"))
-  TransR<-raster(file.path(DataDir,"LandDisturbance/TransR.tif"))
-  SeismicR<-raster(file.path(DataDir,"LandDisturbance/SeismicR.tif"))
-  OilGasR<-raster(file.path(DataDir,"LandDisturbance/OilGasR.tif"))
+  RailR<-raster(file.path(spatialOutDir,"RailR.tif"))
+  RdDensR<-raster(file.path(spatialOutDir,"RdDensR.tif"))
+  TransR<-raster(file.path(spatialOutDir,"TransR.tif"))
+  SeismicR<-raster(file.path(spatialOutDir,"SeismicR.tif"))
+  OilGasR<-raster(file.path(spatialOutDir,"OilGasR.tif"))
 }  
 
-# Provincial CE Data
-# List feature classes in the geodatabase and read in 
-GB_file <- file.path("tmp/GB_Brick")
-if (!file.exists(GB_file)) {
-  GB_gdb <- list.files(file.path(DataDir, "Bears"), pattern = ".gdb", full.names = TRUE)[1]
-  fc_list <- st_layers(GB_gdb)
-  
-  #Mortality
-  Mort <- read_sf(GB_gdb, layer = "COMBINED_Grizzly_PopMort_Allocation_2004_to_2014")
-  # Make a Mortality raster - 1-5 fails - what's the spatial - has had a past mort failure?
-  # CE used hunted units only as failure as it related to policy
-  # Mortr <- Mort[Mort$Pop_Mort_Flag_Hunt == 'Fail' ,] %>% 
-  #  fasterize(ProvRast, background=0)
-  
-  # for status used all mortality 
-  Mortr <- Mort[Mort$Pop_Mort_Flag_v1_allAreas == 'Fail' ,] %>% 
-    fasterize(ProvRast, background=0)
-  
-  #data.frame(Flag=Mort$Pop_Mort_Flag_Hunt,AllN=Mort$Pop_Mort_TOTAL_AllocationP_Count_v1_allAreas)
-  
-  #Front Country
-  FrontCountry <- read_sf(GB_gdb, layer = "FrontCountry_v2_Coastal_DC")
-  # Make a Front Country raster - 3,4,5 area front - 1,2 are back - need to spatialize
-  FrontCountryr <- FrontCountry[FrontCountry$Encounter_Class_Num %in% c(3,4,5) ,] %>% 
-    fasterize(ProvRast, background=0)
-  
-  #Mid Seral
-  MidSeral <- read_sf(GB_gdb, layer = "LU_midSeral_conifer")
-  # Make a Mid Seral raster - 1 is low
-  MidSeralr <- MidSeral[MidSeral$mid_Seral_Num == 1 ,] %>% 
-    fasterize(ProvRast, background=0)
-  
-  # Hunter Day density per km2 LU_hunterDays_annual_per_km2
-  HunterDayD <- read_sf(GB_gdb, layer = "LU_SUMMARY_poly_v5_20160210")
-  # Make a Hunter Day density raster
-  HunterDayDr <- HunterDayD %>% 
-    fasterize(ProvRast, field='LU_hunterDays_annual_per_km2', background=0)
-  
-  #Cow density - see 0.1.Load_Livestock.R
-   
-  #Human density - see 01.Load_Human.R
-  
-  # ratio of historic:recent 2005-2014:all years (including recent) - varies but some to 1940s 
-  # measure of annual average kg biomass - note some units have increased from historic
-  # indicator should be updated with published (PSF) data
-  SalmonChange <- read_sf(GB_gdb, layer = "LU_SUMMARY_poly_v5_20160210")
-  # Make a Salmon raster of per cent negative change - ie a -ve number indicates a positive change
-  SalmonChange$SalmonPc<-(SalmonChange$Tot_Salmon_kg_all-SalmonChange$Tot_Salmon_kg_recent)/SalmonChange$Tot_Salmon_kg_all*100
-  
-  SalmonChangr <- SalmonChange %>% 
-    fasterize(ProvRast, field='SalmonPc', background=0)
-  
-  #Write rasters
-  writeRaster(MidSeralr, filename=file.path(DataDir,"LandDisturbance/MidSeralr.tif"), format="GTiff", overwrite=TRUE)
-  writeRaster(FrontCountryr, filename=file.path(DataDir,"LandDisturbance/FrontCountryr.tif"), format="GTiff", overwrite=TRUE)
-  writeRaster(HunterDayDr, filename=file.path(DataDir,"LandDisturbance/HunterDayDr.tif"), format="GTiff", overwrite=TRUE)
-  writeRaster(Mortr, filename=file.path(DataDir,"LandDisturbance/Mortr.tif"), format="GTiff", overwrite=TRUE)
-  writeRaster(SalmonChangr, filename=file.path(DataDir,"SalmonChangr.tif"), format="GTiff", overwrite=TRUE)
-  
-  
-  #Make a raster stack of GB rasters and save to disk - not using due to raster memory allocation bug
-  GB_Brick<-brick(Mortr, FrontCountryr, MidSeralr) 
-  names(GB_Brick) <- c('Mortr','FrontCountryr','MidSeralr')
-  saveRDS(GB_Brick, file = GB_file)
-  
-  #Organize strata layers
-  #BEI
-  BEI <- read_sf(GB_gdb, layer = "Final_Grizzly_BEI")
-  # Make a BEI raster
-  BEI_1_2_r <- BEI[BEI$HIGHCAP %in% c(1,2) ,] %>% 
-    fasterize(ProvRast, background=NA)
-  BEI_1_5_r <- BEI[BEI$HIGHCAP %in% c(1,2,3,4,5) ,] %>% 
-    fasterize(ProvRast, background=NA)
-  BEIr <- fasterize(BEI, ProvRast, background=0, field='HIGHCAP')
-  #GBPU
-  GBPU <- read_sf(GB_gdb, layer = "GBPU_BC_edits_v2_20150601")
-  GBPU_lut <- tidyr::replace_na(data.frame(GRIZZLY_BEAR_POP_UNIT_ID=GBPU$GRIZZLY_BEAR_POP_UNIT_ID, POPULATION_NAME=GBPU$POPULATION_NAME, stringsAsFactors = FALSE), list(POPULATION_NAME = 'extirpated'))
-  saveRDS(GBPU, file = 'tmp/GBPU')
-  saveRDS(GBPU_lut, file = file.path(DataDir,'GBPU_lut'))
-  
-  # Make a GBPU raster
-  GBPUr <- fasterize(GBPU, ProvRast, field = 'GRIZZLY_BEAR_POP_UNIT_ID', background=NA)
-  writeRaster(GBPUr, filename=file.path(DataDir,"Strata/GBPUr.tif"), format="GTiff", overwrite=TRUE)
-  #set GBPUr to NA where BEI is NonHab
-  GBPUr_NonHab <- overlay(GBPUr, NonHab, fun = function(x, y) {
-    x[!is.na(y[])] <- NA
-    return(x)
-  })
-  writeRaster(GBPUr_NonHab, filename=file.path(DataDir,"Strata/GBPUr_NonHab.tif"), format="GTiff", overwrite=TRUE)
-  
-  GBPUr_BEI_1_2 <- overlay(GBPUr_NonHab, BEI_1_2_r, fun = function(x, y) {
-    x[is.na(y[])] <- NA
-    return(x)
-  })
-  writeRaster(GBPUr_BEI_1_2, filename=file.path(DataDir,"Strata/GBPUr_BEI_1_2.tif"), format="GTiff", overwrite=TRUE)
-  
-  GBPUr_BEI_1_5 <- overlay(GBPUr_NonHab, BEI_1_5_r, fun = function(x, y) {
-    x[is.na(y[])] <- NA
-    return(x)
-  })
-  writeRaster(GBPUr_BEI_1_5, filename=file.path(DataDir,"Strata/GBPUr_BEI_1_5.tif"), format="GTiff", overwrite=TRUE)
-  
-  #Landform from AdaptWest
-  # Need to pull strata of interest - valley bottom
-  LForm_file <- file.path(dataOutDir,"LForm.tif")
-  LFormFlat_file <- file.path(dataOutDir,"LFormFlat.tif")
-  if (!file.exists(LForm_file)) {
-    LForm<-mask(raster(file.path(DataDir,"Landform_BCAlbs.tif")) %>%
-                  resample(ProvRast, method='ngb'), BCr)
-    
-    LF_lut<-read_csv(file.path(DataDir,'landform_lut.csv'), col_names=TRUE)
-    writeRaster(LForm, filename=file.path(DataDir,"LForm.tif"), format="GTiff", overwrite=TRUE)
-    saveRDS(LF_lut, file = 'tmp/LF_lut')
-    
-    # Pull out just the flat areas - valley bottom (1000) and plains (5000)
-    LFormFlat<-LForm
-    LFormFlat[!(LFormFlat[] %in% c(1000,5000,6000,7000,8000))]<-NA
-    writeRaster(LFormFlat, filename=file.path(DataDir,"Strata/LFormFlat.tif"), format="GTiff", overwrite=TRUE)
-    # Pull out really flat areas - valley bottom (1000) and plains (5000)
-    LFormFlatFlat<-LForm
-    LFormFlatFlat[!(LFormFlatFlat[] %in% c(1000,5000))]<-NA
-    writeRaster(LFormFlatFlat, filename=file.path(DataDir,"Strata/LFormFlatFlat.tif"), format="GTiff", overwrite=TRUE)
-  } else {
-    LForm <- raster(file.path(DataDir,"LForm.tif"))
-    LFormFlat <- raster(file.path(DataDir,"Strata/LFormFlat.tif"))
-    LFormFlatFlat <- raster(file.path(DataDir,"Strata/LFormFlatFlat.tif"))
-    LF_lut<- readRDS(file = 'tmp/LF_lut')
-  }
-  
-  GBPUr_LFormFlat <- overlay(GBPUr_NonHab, LFormFlat, fun = function(x, y) {
-    x[is.na(y[])] <- NA
-    return(x)
-  })
-  writeRaster(GBPUr_LFormFlat, filename=file.path(DataDir,"Strata/GBPUr_LFormFlat.tif"), format="GTiff", overwrite=TRUE)
-  
-  GBPUr_LFormFlatFlat <- overlay(GBPUr_NonHab, LFormFlatFlat, fun = function(x, y) {
-    x[is.na(y[])] <- NA
-    return(x)
-  })
-  writeRaster(GBPUr_LFormFlatFlat, filename=file.path(DataDir,"Strata/GBPUr_LFormFlatFlat.tif"), format="GTiff", overwrite=TRUE)
-  
-  #Read in landcover
-  LandCover<-raster(file.path(DataDir,"LandCover/land_cover_n_age_2017.tif"))
-  LC_lut<-read_csv(file.path(DataDir,'LandCover/LandCover_lut.csv'), col_names=TRUE)
-  Forest<-LandCover
-  Forest[!(Forest[] > 0)]<-NA
-  GBPUr_Forest <- overlay(GBPUr_NonHab, Forest, fun = function(x, y) {
-    x[is.na(y[])] <- NA
-    return(x)
-  })
-  writeRaster(GBPUr_Forest, filename=file.path(DataDir,"Strata/GBPUr_Forest.tif"), format="GTiff", overwrite=TRUE)
-  
-  #######
+#Load Strata
+# from GB_Data repo: https://github.com/bcgov/GB_Data
+NonHab<-raster(file.path(StrataDir,"NonHab.tif"))
+GBPUr<-raster(file.path(StrataDir,"GBPUr.tif"))
+WMUr<-raster(file.path(StrataDir,"WMUr.tif"))
+WMUr_NonHab<-raster(file.path(StrataDir,"WMUr_NonHab.tif"))
+GBPUr_NonHab<-raster(file.path(StrataDir,"GBPUr_NonHab.tif"))
+GBPUr_BEI_1_2<-raster(file.path(StrataDir,"GBPUr_BEI_1_2.tif"))
+GBPUr_BEI_1_5<-raster(file.path(StrataDir,"GBPUr_BEI_1_5.tif"))
+GBPUr_LFormFlat<-raster(file.path(StrataDir,"GBPUr_LFormFlat.tif"))
+GBPUr_LFormFlatFlat<-raster(file.path(StrataDir,"GBPUr_LFormFlatFlat.tif"))
+GBPUr_Forest<-raster(file.path(StrataDir,"GBPUr_Forest.tif"))
+
+    #######
   #Other possible layers - not currently used
   #GB CE summary data
   GB <- read_sf(GB_gdb, layer = "LU_SUMMARY_poly_v5_20160210")
@@ -313,18 +166,6 @@ if (!file.exists(GB_file)) {
   
   # Make a LU_summary raster
   GB_CEr <- fasterize(GB, ProvRast, field = 'LANDSCAPE_UNIT_PROVID')
-  
-} else {
-  NonHab<-raster(file.path(DataDir,"NonHab.tif"))
-  GBPUr<-raster(file.path(DataDir,"Strata/GBPUr.tif"))
-  GBPUr_NonHab<-raster(file.path(DataDir,"Strata/GBPUr_NonHab.tif"))
-  GBPUr_BEI_1_2<-raster(file.path(DataDir,"Strata/GBPUr_BEI_1_2.tif"))
-  GBPUr_BEI_1_5<-raster(file.path(DataDir,"Strata/GBPUr_BEI_1_5.tif"))
-  GBPUr_LFormFlat<-raster(file.path(DataDir,"Strata/GBPUr_LFormFlat.tif"))
-  GBPUr_LFormFlatFlat<-raster(file.path(DataDir,"Strata/GBPUr_LFormFlatFlat.tif"))
-  GBPUr_Forest<-raster(file.path(DataDir,"Strata/GBPUr_Forest.tif"))
-  GB_Brick <- readRDS(file = GB_file)
-}  
 
 #Read in LU csv 
 LU_Summ_in <- data.frame(read.csv(header=TRUE, file=paste(DataDir, "/Bears/GBear_LU_Summary_scores_v5_20160823.csv", sep=""), sep=",", strip.white=TRUE, ))
--- 02_clean.R
+++ 02_clean.R
@@ -14,26 +14,18 @@ source("header.R")
 
 #Identify Strata layers - read from disk for analysis
 StrataL <- c('GBPUr','GBPUr_NonHab','GBPUr_BEI_1_2','GBPUr_BEI_1_5','GBPUr_LFormFlat')
-GBPU_lut<-readRDS(file = file.path(DataDir,'GBPU_lut'))
+GBPU_lut<-readRDS(file = file.path(StrataDir,'GBPU_lut'))
 
 # Residential and Commercial Development - Threat 1
 # BTM 'Urban'
-Residential_1a <-raster(file.path(DataDir,"LandDisturbance/Urban.tif"))
-Residential_1b <-raster(file.path(DataDir,"HumanDensityR.tif"))
+Residential_1a <-UrbanR
+Residential_1b <-HumanDensityR
 
 # Agriculture and Range - Threat 2
 # BTM - 'Agriculture', 'Residential Agriculture Mixtures', 'Range Lands'; Stats Can - livestock density
-Agriculture_2.1 <- raster(file.path(DataDir,"LandDisturbance/AgricultureR.tif"))
-Agriculture_2.3a <- raster(file.path(DataDir,"LandDisturbance/RangeR.tif"))
-Agriculture_2.3b<- raster(file.path(DataDir,"LandDisturbance/LSDensityR.tif"))
-
-if (!file.exists(file.path(DataDir,"LandDisturbance/Agriculture_2all.tif"))) {
-  Agriculture_2all<- Agriculture_2.1+Agriculture_2.3a
-  Agriculture_2all[Agriculture_2all[]>0]<-1
-  writeRaster(Agriculture_2all, filename=file.path(DataDir,"LandDisturbance/Agriculture_2all.tif"), format="GTiff", overwrite=TRUE)
-} else {
-  Agriculture_2all<- raster(file.path(DataDir,"LandDisturbance/Agriculture_2all.tif"))
-}
+Agriculture_2.1 <- AgricultureR
+Agriculture_2.3a <- RangeR
+Agriculture_2.3b<- LivestockDensityR
 
 # Energy Production and Mining - Threat 3
 # BTM - 'Mining'
@@ -61,87 +53,88 @@ Energy_3<- merge(Energy_3.1, Energy_3.2, by='GRIZZLY_BEAR_POP_UNIT_ID', all=TRUE
 Energy_3[is.na(Energy_3)] <- 0
 colnames(Energy_3)<-c('GRIZZLY_BEAR_POP_UNIT_ID','Energy_3.1','Energy_3.2','Energy_3.3')
 Energy_3$Energy_3all<-(Energy_3$Energy_3.1+Energy_3$Energy_3.2+Energy_3$Energy_3.3)
-saveRDS(Energy_3, file = (file.path(DataDir,'Energy_3')))
+saveRDS(Energy_3, file = (file.path(spatialOutDir,'Energy_3')))
 
 # Transporation & Services Corridors - Threat 4
 
-if (!file.exists(file.path(DataDir,"LandDisturbance/Transport_4.1.tif"))) {
+if (!file.exists(file.path(spatialOutDir,"Transport_4.1.tif"))) {
   # Focal function to caluclate a 1km diameter cirular window radius - 564.9769748m
   # multiply result by 0.1 to get km/km2 for each cell
   T1<-Reduce("+",list(RdDensR,RailR))
   T1[T1>0]<-1
   Transport_4.1L<-T1
-  writeRaster(Transport_4.1L, filename=file.path(DataDir,"LandDisturbance/Transport_4.1L.tif"), format="GTiff", overwrite=TRUE)
+  writeRaster(Transport_4.1L, filename=file.path(spatialOutDir,"Transport_4.1L.tif"), format="GTiff", overwrite=TRUE)
   fw<-focalWeight(raster(res=c(100,100)),565,type='circle')
   fw[fw>0]<-1
   T2 <- focal(T1, w=as.matrix(fw), fun='sum', na.rm=FALSE, pad=TRUE) # to make it km/km2 max should 9.7 km/km2
-  #writeRaster(T2, filename=file.path(DataDir,"LandDisturbance/T2.tif"), format="GTiff", overwrite=TRUE)
+  #writeRaster(T2, filename=file.path(spatialOutDir,"T2.tif"), format="GTiff", overwrite=TRUE)
   #Flag if linear density is > 0.6km/km2, where 1=100m=0.1km
   #Transport_4.1 <- reclassify(T2, c(0,0.6,0,  0.6,200,1))
   #Transport_4.1 <- reclassify(T2, c(0,6,0,  6,200,1))
   Transport_4.1 <- T2
-  writeRaster(Transport_4.1, filename=file.path(DataDir,"LandDisturbance/Transport_4.1.tif"), format="GTiff", overwrite=TRUE)
+  writeRaster(Transport_4.1, filename=file.path(spatialOutDir,"Transport_4.1.tif"), format="GTiff", overwrite=TRUE)
   #Note: function to make a circular matrix for focal function, from:
   #https://scrogster.wordpress.com/2012/10/05/applying-a-circular-moving-window-filter-to-raster-data-in-r/
   #not required using focalWeight instead
 } else {
-  Transport_4.1L<-raster(file.path(DataDir,"LandDisturbance/Transport_4.1L.tif"))
-  Transport_4.1<-raster(file.path(DataDir,"LandDisturbance/Transport_4.1.tif"))
+  Transport_4.1L<-raster(file.path(spatialOutDir,"Transport_4.1L.tif"))
+  Transport_4.1<-raster(file.path(spatialOutDir,"Transport_4.1.tif"))
 }
 
-if (!file.exists(file.path(DataDir,"LandDisturbance/Transport_4.2.tif"))) {
+if (!file.exists(file.path(spatialOutDir,"Transport_4.2.tif"))) {
   # Focal function to caluclate a 1km diameter cirular window radius - 564.9769748m
   # multiply result by 0.1 to get km/km2 for each cell
   #T42<-Reduce("+",list(OilGasR,TransR))
   T42<-Reduce("+",list(OilGasR,TransR,SeismicR))
   T42[T42[]>0]<-1
   Transport_4.2L<-T42
-  writeRaster(Transport_4.2L, filename=file.path(DataDir,"LandDisturbance/Transport_4.2L.tif"), format="GTiff", overwrite=TRUE)
+  writeRaster(Transport_4.2L, filename=file.path(spatialOutDir,"Transport_4.2L.tif"), format="GTiff", overwrite=TRUE)
   #Note: function to make a circular matrix for focal function, from:
   #https://scrogster.wordpress.com/2012/10/05/applying-a-circular-moving-window-filter-to-raster-data-in-r/
   #not required using focalWeight instead
 } else {
-  Transport_4.2L<-raster(file.path(DataDir,"LandDisturbance/Transport_4.2L.tif"))
+  Transport_4.2L<-raster(file.path(spatialOutDir,"Transport_4.2L.tif"))
 }
 
 #Combined Threat 4
-if (!file.exists(file.path(DataDir,"LandDisturbance/Transport_4all.tif"))) {
+if (!file.exists(file.path(spatialOutDir,"Transport_4all.tif"))) {
   # Focal function to caluclate a 1km diameter cirular window radius - 564.9769748m
   # multiply result by 0.1 to get km/km2 for each cell
   Tall1<-Reduce("+",list(RdDensR,RailR,OilGasR,TransR,SeismicR))
   Tall1[Tall1>0]<-1
   Transport_4allL<-Tall1
-  writeRaster(Transport_4allL, filename=file.path(DataDir,"LandDisturbance/Transport_4allL.tif"), format="GTiff", overwrite=TRUE)
+  writeRaster(Transport_4allL, filename=file.path(spatialOutDir,"Transport_4allL.tif"), format="GTiff", overwrite=TRUE)
   fw<-focalWeight(raster(res=c(100,100)),565,type='circle')
   fw[fw>0]<-1
   #T2 <- focal(T1, w=as.matrix(fw[fw>0]<-1), fun='sum', na.rm=FALSE, pad=TRUE)
   Tall2 <- focal(Tall1, w=as.matrix(fw), fun='sum', na.rm=FALSE, pad=TRUE)
-  #writeRaster(Tall2, filename=file.path(DataDir,"LandDisturbance/T2all.tif"), format="GTiff", overwrite=TRUE)
+  #writeRaster(Tall2, filename=file.path(spatialOutDir,"T2all.tif"), format="GTiff", overwrite=TRUE)
   
   #Flag if linear density is > 0.6km/km2
   #Transport_4all <- reclassify(Tall2, c(0,0.6,0,0.6,200,1))
   Transport_4all<-Tall2
-  writeRaster(Transport_4all, filename=file.path(DataDir,"LandDisturbance/Transport_4all.tif"), format="GTiff", overwrite=TRUE)
+  writeRaster(Transport_4all, filename=file.path(spatialOutDir,"Transport_4all.tif"), format="GTiff", overwrite=TRUE)
   #Note: function to make a circular matrix for focal function, from:
   #https://scrogster.wordpress.com/2012/10/05/applying-a-circular-moving-window-filter-to-raster-data-in-r/
   #not required using focalWeight instead
 } else {
-  Transport_4all<-raster(file.path(DataDir,"LandDisturbance/Transport_4all.tif"))
-  Transport_4allL<-raster(file.path(DataDir,"LandDisturbance/Transport_4allL.tif"))
+  Transport_4all<-raster(file.path(spatialOutDir,"Transport_4all.tif"))
+  Transport_4allL<-raster(file.path(spatialOutDir,"Transport_4allL.tif"))
 }
 
 # Biological Resource Use - Threat 5
 # CE Mortality overages
-BioUse_5.1a <- raster(file.path(DataDir,"LandDisturbance/Mortr.tif"))
-BioUse_5.1b <- raster(file.path(DataDir,"LandDisturbance/HunterDayDr.tif"))
-BioUse_5.3 <- raster(file.path(DataDir,"LandDisturbance/MidSeralr.tif"))
+BioUse_5.1a <- MortR
+BioUse_5.1b <- HuntDDensR
+BioUse_5.3 <- MidSeralR
+
 
 # Human Intrusions - Threat 6
 # CE Front Country Indicator - cell flagged as front country (1) or not (0)
-HumanIntrusion_6 <- raster(file.path(DataDir,"LandDisturbance/FrontCountryr.tif"))
+HumanIntrusion_6 <- FrontCountryR
 
 # Salmon Decline - Climate Change - Threat 11
-ClimateChange_11 <-raster(file.path(DataDir,"SalmonChangr.tif"))
+ClimateChange_11 <-SalmonChangeR
 
 #Make a Threat brick for assessment
 ThreatBrick <- stack(Residential_1a, Residential_1b,Agriculture_2.1,Agriculture_2.3a,Agriculture_2.3b,Agriculture_2all,Transport_4.1,Transport_4.1L,Transport_4.2L,Transport_4all,Transport_4allL,BioUse_5.1a,BioUse_5.1b,BioUse_5.3,HumanIntrusion_6, ClimateChange_11)
